d80f372d00e2495f96b7361b79ecbcbef728b612
Karolina Mathias <160190348+KahMath@users.noreply.github.com>
2024-03-01 19:46:33 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,110 @@
 const form = document.querySelector("form");
 const login = document.getElementById("login");
+const email = document.getElementById("email");
 const senha = document.getElementById("senha");
-const loginError = document.querySelector("#login + span.error");
-const senhaError = document.querySelector("#senha + span.error");
+const confirmSenha = document.getElementById("confirmSenha");
+const loginErro = document.querySelector("#login + span.erro");
+const emailErro = document.querySelector("#email + span.erro");
+const senhalErro = document.querySelector("#senha + span.erro");
+const confirmSenhalErro = document.querySelector("#confirmSenha + span.erro")
 const botao = document.getElementById("botao");
 const paragrafo = document.querySelector("p");
 
 login.addEventListener("input", (event) => {
     if (login.validity.valid) {
-        loginError.textContent = "";
-        loginError.className = "error";
+        loginErro.textContent = "";
+        loginErro.className = "erro";
     } else {
-        showError(login, loginError);
+        mostrarErro(login, loginErro);
     }
 });
 
+email.addEventListener("input", (event) => {
+    if (email.validity.valid) {
+        emailErro.textContent = "";
+        emailErro.className = "erro";
+    } else {
+        mostrarErro(email, emailErro)
+    }
+})
+
 senha.addEventListener("input", (event) => {
     if (senha.validity.valid) {
-        senhaError.textContent = "";
-        senhaError.className = "error";
+        senhalErro.textContent = "";
+        senhalErro.className = "erro";
     } else {
-        showError(senha, senhaError);
+        mostrarErro(senha, senhalErro);
+    }
+});
+
+confirmSenha.addEventListener("input", (event) => {
+    if (confirmSenha.validity.valid) {
+        confirmSenhalErro.textContent = "";
+        confirmSenhalErro.className = "erro";
+    } else {
+        mostrarErro(confirmSenha, confirmSenhalErro);
     }
 });
 
 form.addEventListener("submit", (event) => {
     if (!login.validity.valid) {
-        showError(login, loginError);
+        mostrarErro(login, loginErro);
+        event.preventDefault();
+    } else if (!email.validity.valid) {
+        mostrarErro(email, emailErro);
         event.preventDefault();
     } else if (!senha.validity.valid) {
-        showError(senha, senhaError);
+        mostrarErro(senha, senhalErro);
+        event.preventDefault();
+    } else if (!confirmSenha.validity.valid) {
+        mostrarErro(confirmSenha, confirmSenhalErro);
+        event.preventDefault();
+    } else if (valideConfirmSenha() === true) {
         event.preventDefault();
     }
 });
 
-function showError(input, errorElement) {
+function mostrarErro(input, erroElement) {
     if (input.validity.valueMissing) {
-        errorElement.textContent = `Você precisa inserir um ${input.id === "login" ? "login" : "senha"}.`;
+        if (input.id === "login") {
+            erroElement.textContent = `Você precisa inserir um ${input.id}.`;
+        } else if (input.id === "email") {
+            erroElement.textContent = 'Você precisa inserir um e-mail.';
+        } else if (input.id === "senha") {
+            erroElement.textContent = `Você precisa inserir uma ${input.id}.`;
+        } else if (input.id === "confirmSenha") {
+            erroElement.textContent = `Você precisa confirmar a senha.`;
+        }
+    } else if (input.validity.typeMismatch) {
+        erroElement.textContent = 'Formato de e-mail inválido.';
     } else if (input.validity.tooShort) {
-        errorElement.textContent = `${input.id === "login" ? "Login" : "Senha"} precisa ter no mínimo ${input.minLength} caracteres.`;
+        if (input.id === "login") {
+            erroElement.textContent = `Login precisa ter no mínimo ${input.minLength} caracteres.`;
+        } else if (input.id === "senha") {
+            erroElement.textContent = `Senha precisa ter no mínimo ${input.minLength} caracteres.`;
+        }
     }
 
-    errorElement.className = "error active";
+    erroElement.className = "erro ativo";
+}
+
+function valideConfirmSenha() {
+    if (confirmSenha.value !== senha.value) {
+        confirmSenhalErro.textContent = "As senhas não coincidem.";
+        confirmSenhalErro.className = "erro ativo";
+        return true;
+    }
 }
 
 botao.addEventListener("click", mensagem);
 
+
 function mensagem() {
-    if (login.validity.valueMissing) {
-        loginError.textContent = "Você precisa inserir um login.";
-    } else if (login.validity.tooShort) {
-        loginError.textContent = `Login precisa ter no mínimo ${login.minLength} caracteres.`;
-    } else if (botao.value === "Esqueceu a senha?") {
+    if (email.validity.valueMissing) {
+        emailErro.textContent = "Você precisa inserir um email.";
+    } else if (email.validity.typeMismatch) {
+        emailErro.textContent = 'Formato de e-mail inválido.';
+    } else {
         paragrafo.textContent = "E-mail de recuperação de senha enviado!";
     }
 }
